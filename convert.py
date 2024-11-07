c9174da69e6bba1f69e1a7e2de0e2e9418b760cf
GelbEinhalb <exzitonyw@gmail.com>
2024-11-07 16:58:57 +0100

    add convert.py

---
+++ convert.py
@@ -0,0 +1,17 @@
+import os
+from PIL import Image
+
+
+def is_raw_file(filepath, add_text):
+    raw_extensions = ('.cr2', '.nef', '.arw', '.dng', '.raw')
+    is_raw = filepath.lower().endswith(raw_extensions)
+    if not is_raw:
+        add_text(f"[INFO] skipping \"{filepath}\"")
+    return is_raw
+
+
+def convert(filepath, destination, add_text):
+    if is_raw_file(filepath, add_text):
+        with Image.open(filepath) as img:
+            tiff_path = os.path.join(destination, os.path.splitext(os.path.basename(filepath))[0] + '.tiff')
+            img.save(tiff_path, format='TIFF')
