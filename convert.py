182340df42929caad6ed2e408130e1ff9c984a02
GelbEinhalb <exzitonyw@gmail.com>
2024-11-07 18:12:50 +0100

    fix (#1) and (#2)

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
+import rawpy
+import imageio
 import os
-from PIL import Image
-
 
 def is_raw_file(filepath, destination, add_text):
     raw_extensions = ('.cr2', '.nef', '.arw', '.dng', '.raw')
@@ -9,10 +9,19 @@ def is_raw_file(filepath, destination, add_text):
         add_text(f"[INFO] skipping \"{filepath}\"", destination)
     return is_raw
 
-
 def convert(filepath, destination, add_text):
     if is_raw_file(filepath, destination, add_text):
-        add_text(f"[INFO] converting \"{filepath}\" to tiff", destination)
-        with Image.open(filepath) as img:
+        add_text(f"[INFO] converting \"{filepath}\" to TIFF using imageio", destination)
+
+        with rawpy.imread(filepath) as raw:
+
+            rgb = raw.postprocess(
+                output_bps=16,
+                no_auto_bright=True,
+                use_camera_wb=True
+            )
+
             tiff_path = os.path.join(destination, os.path.splitext(os.path.basename(filepath))[0] + '.tiff')
-            img.save(tiff_path, format='TIFF', compression='tiff_lzw')
+
+            imageio.imwrite(filepath=tiff_path, im=rgb, format='TIFF')
+            add_text(f"[INFO] saved \"{tiff_path}\" as uncompressed 16-bit TIFF", destination)
