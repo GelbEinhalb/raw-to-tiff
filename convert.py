20441b152537f2a4545081192fef4e3b685815c5
GelbEinhalb <exzitonyw@gmail.com>
2024-11-07 17:08:27 +0100

    Add logging

--- convert.py
+++ convert.py
@@ -2,16 +2,17 @@ import os
 from PIL import Image
 
 
-def is_raw_file(filepath, add_text):
+def is_raw_file(filepath, destination, add_text):
     raw_extensions = ('.cr2', '.nef', '.arw', '.dng', '.raw')
     is_raw = filepath.lower().endswith(raw_extensions)
     if not is_raw:
-        add_text(f"[INFO] skipping \"{filepath}\"")
+        add_text(f"[INFO] skipping \"{filepath}\"", destination)
     return is_raw
 
 
 def convert(filepath, destination, add_text):
-    if is_raw_file(filepath, add_text):
+    if is_raw_file(filepath, destination, add_text):
+        add_text(f"[INFO] converting \"{filepath}\" to tiff", destination)
         with Image.open(filepath) as img:
             tiff_path = os.path.join(destination, os.path.splitext(os.path.basename(filepath))[0] + '.tiff')
             img.save(tiff_path, format='TIFF')
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 import tkinter as tk
 from tkinter import scrolledtext
 from tkinter import filedialog
+
+import convert
 
 
 class App:
@@ -8,25 +11,64 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Camera Raw to Tiff")
-        self.root.geometry("600x400")
+        self.root.geometry("900x400")
         self.root.resizable(False, False)
 
-        self.console = scrolledtext.ScrolledText(root, wrap=tk.WORD, height=20, width=70)
+        self.console = scrolledtext.ScrolledText(root, wrap=tk.WORD, height=20, width=120)
         self.console.pack(padx=10, pady=10)
         self.console.config(state=tk.DISABLED)
 
         self.button = tk.Button(root, text="Start", command=self.convert)
         self.button.pack(pady=(0, 10))
 
-    def convert(self) -> None:
-        folder_selected = filedialog.askdirectory()
-        self.add_text(f"Selected folder {folder_selected}")
+        self.saved_logs = []
 
-    def add_text(self, text):
+    def convert(self) -> None:
+        folder_path = filedialog.askdirectory()
+        destination_path = os.path.join(folder_path, "tiff")
+
+        self.add_text(f"[INFO] Selected folder {folder_path}")
+
+        if folder_path == "" or not os.path.exists(folder_path):
+            self.add_text(f"[ERROR] You need to select a valid folder")
+            return
+
+        if not os.path.exists(destination_path):
+            os.mkdir(destination_path)
+
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file)
+
+            if os.path.isdir(file_path):
+                continue
+
+            self.add_text(f"[INFO] found \"{file_path}\"", destination_path)
+            convert.convert(file_path, destination_path, self.add_text)
+
+        self.add_text(f"[INFO] FINISHED", destination_path)
+
+
+    def add_text(self, text, destination = None):
+        if destination is None:
+            self.saved_logs.append(text)
+        else:
+            if len(self.saved_logs) > 0:
+                self.saved_logs.append(text)
+                for log in self.saved_logs:
+                    self.write_log_to_file(log, destination)
+                self.saved_logs.clear()
+            else:
+                self.write_log_to_file(text, destination)
+
         self.console.config(state=tk.NORMAL)
         self.console.insert(tk.END, text + "\n")
         self.console.see(tk.END)
         self.console.config(state=tk.DISABLED)
+
+    @staticmethod
+    def write_log_to_file(text, destination):
+        with open(os.path.join(destination, "log.txt"), "a") as file:
+            file.write(text + "\n")
 
 
 if __name__ == "__main__":
