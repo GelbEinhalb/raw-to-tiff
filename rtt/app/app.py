df63132d038d921b777f2f6325a8cb3dc987cb61
GelbEinhalb <exzitonyw@gmail.com>
2024-11-10 18:08:52 +0100

    Add threading

--- rtt/app/app.py
+++ rtt/app/app.py
@@ -1,26 +1,64 @@
-import tkinter as tk
+import os
+import log
+import time
+from threading import Thread
+from converter.convert import convert
+
+import tkinter
 from tkinter import scrolledtext
 from tkinter import filedialog
 
-import log
-
+from typing import Dict
+from typing import List
 
 class App:
-
     def __init__(self, root):
         self.root = root
-        self.root.title("Camera Raw to Tiff")
+        self.root.title("Raw to Tiff")
         self.root.geometry("600x400")
         self.root.resizable(True, False)
 
-        self.console: scrolledtext.ScrolledText = scrolledtext.ScrolledText(root, wrap=tk.WORD, height=20, width=120)
-        self.console.config(state=tk.DISABLED)
-        self.console.pack(padx=10, pady=10, fill=tk.X)
+        # Console for logging
+        self.console: scrolledtext.ScrolledText = scrolledtext.ScrolledText(root, wrap=tkinter.WORD, height=20, width=120)
+        self.console.config(state=tkinter.DISABLED)
+        self.console.pack(padx=10, pady=10, fill=tkinter.X)
         log.set_console(self.console)
 
-        self.button: tk.Button = tk.Button(root, text="Start", command=self.convert)
-        self.button.pack(padx=10, pady=10, fill=tk.X)
+        # Start button
+        self.button: tkinter.Button = tkinter.Button(root, text="Start", command=self.start_conversion)
+        self.button.pack(padx=10, pady=10, fill=tkinter.X)
+
+        self.label: tkinter.Label = tkinter.Label(root, text="Idle")
+        self.label.pack(pady=10)
 
 
-    def convert(self) -> None:
-        log.debug("test")
+    def start_conversion(self):
+        start_time: float = time.time()
+        self.button.config(state=tkinter.DISABLED)
+
+        # Select the folder and set up the destination
+        folder_path: str = filedialog.askdirectory()
+        destination: str = os.path.join(folder_path, "tiff")
+
+        log.debug(f"Selected folder \"{folder_path}\"")
+
+        if not folder_path or not os.path.exists(folder_path):
+            log.error("You need to select a valid folder path")
+            return
+
+        if not os.path.exists(destination):
+            log.debug("Creating TIFF location")
+            os.mkdir(destination)
+
+        files: List[str] = os.listdir(folder_path)
+        file_count: int = len(files)
+
+        for file in files:
+            file_path = os.path.join(folder_path, file)
+
+            thread: Thread = Thread(target=convert, args=(file_path, destination), daemon=True)
+            thread.start()
+
+        total_time = time.time() - start_time
+        log.debug(f"Total conversion time: {total_time:.2f} seconds")
+        self.button.config(state=tkinter.NORMAL)
