b82a063c07ecfa142eff89a43dfcc117065c1f9d
GelbEinhalb <exzitonyw@gmail.com>
2024-11-10 19:18:14 +0100

    change convert.py to work with threads

--- rtt/converter/convert.py
+++ rtt/converter/convert.py
@@ -1,27 +1,25 @@
 import rawpy
 import imageio
 import os
+import log
 
-def is_raw_file(filepath, destination, add_text):
+def is_raw_file(path):
     raw_extensions = ('.cr2', '.nef', '.arw', '.dng', '.raw')
-    is_raw = filepath.lower().endswith(raw_extensions)
+    is_raw = path.lower().endswith(raw_extensions)
     if not is_raw:
-        add_text(f"[INFO] skipping \"{filepath}\"", destination)
+        log.debug(f"Skipping \"{path}\"")
     return is_raw
 
-def convert(filepath, destination, add_text):
-    if is_raw_file(filepath, destination, add_text):
-        add_text(f"[INFO] converting \"{filepath}\" to TIFF using imageio", destination)
-
-        with rawpy.imread(filepath) as raw:
-
+def convert(path, destination, increment):
+    increment()
+    if is_raw_file(path):
+        with rawpy.imread(path) as raw:
             rgb = raw.postprocess(
                 output_bps=16,
                 no_auto_bright=True,
                 use_camera_wb=True
             )
 
-            tiff_path = os.path.join(destination, os.path.splitext(os.path.basename(filepath))[0] + '.tiff')
-
+            tiff_path = os.path.join(destination, os.path.splitext(os.path.basename(path))[0] + '.tiff')
             imageio.imwrite(uri=tiff_path, im=rgb, format='TIFF')
-            add_text(f"[INFO] saved \"{tiff_path}\" as uncompressed 16-bit TIFF", destination)
+            log.info(f"Converted \"{path}\"")
