3f34a80d6c12c7d9706b322f7f79638e9e4b487a
GelbEinhalb <exzitonyw@gmail.com>
2024-11-10 19:40:37 +0100

    stop thread pool after window close

--- rtt/app/app.py
+++ rtt/app/app.py
@@ -1,4 +1,5 @@
 import os
+
 import log
 import time
 from threading import Thread
@@ -16,14 +17,17 @@ class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Raw to Tiff")
-        self.root.geometry("800x400")
+        self.root.geometry("850x500")
         self.root.resizable(True, False)
 
         # Console for logging
-        self.console: scrolledtext.ScrolledText = scrolledtext.ScrolledText(root, wrap=tkinter.WORD, height=18, width=120)
+        self.console: scrolledtext.ScrolledText = scrolledtext.ScrolledText(root, wrap=tkinter.WORD, height=25, width=120)
         self.console.config(state=tkinter.DISABLED)
         self.console.pack(padx=10, pady=10, fill=tkinter.X)
         log.set_console(self.console)
+
+        log.welcome("Click the \"Start\" button below to select a folder!")
+        log.welcome("You can cancel the conversion at any time by closing this window.")
 
         # Status Label
         self.label = tkinter.Label(root, text="Idle")
@@ -38,6 +42,13 @@ class App:
         self.image_count: int = 0
         self.lock: Lock = Lock()
 
+        cpu_count: int = os.cpu_count()
+        worker_count: int = max(1, (cpu_count - 1))
+        self.executor = ThreadPoolExecutor(max_workers=worker_count)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        log.debug(f"detected {cpu_count} cpu cores")
+        log.debug(f"conversion will run on {worker_count} worker threads")
+
     def increment(self) -> None:
         with self.lock:
             self.count += 1
@@ -49,7 +60,7 @@ class App:
                 self.root.after(0, lambda: self.button.config(state=tkinter.NORMAL))
 
     def update_status_label(self):
-        self.label.config(text=f"Finished {self.count} / {self.image_count} Images")
+        self.label.config(text=f"finished {self.count} / {self.image_count} images")
 
     def start_conversion(self):
         self.count = 0
@@ -59,26 +70,29 @@ class App:
         path: str = filedialog.askdirectory()
         destination: str = os.path.join(path, "tiff")
 
-        log.debug(f"Selected folder \"{path}\"")
+        log.debug(f"selected folder \"{path}\"")
 
         if not path or not os.path.exists(path):
-            log.error("You need to select a valid folder path")
+            log.error("you need to select a valid folder path")
             self.button.config(state=tkinter.NORMAL)
             return
 
         if not os.path.exists(destination):
-            log.debug("Creating TIFF location")
+            log.debug("creating TIFF location")
             os.mkdir(destination)
         log.set_location(destination)
 
         files: List[str] = os.listdir(path)
         self.image_count: int = len(files)
 
-
         Thread(target=self.background, args=(files, path, destination), daemon=True).start()
 
     def background(self, files, path, destination):
-        with ThreadPoolExecutor(max_workers=8) as executor:
-            for file in files:
-                file_path = os.path.join(path, file)
-                executor.submit(convert, file_path, destination, self.increment)
+        for file in files:
+            file_path = os.path.join(path, file)
+            self.executor.submit(convert, file_path, destination, self.increment)
+        self.root.after(0, lambda: self.button.config(state=tkinter.NORMAL))
+
+    def on_close(self):
+        self.executor.shutdown(wait=False)
+        self.root.destroy()
