84523f0db9137dadeefa398732a78adacf2c602e
GelbEinhalb <exzitonyw@gmail.com>
2024-11-10 19:17:58 +0100

    switch to using ThreadPoolExecutor

--- rtt/app/app.py
+++ rtt/app/app.py
@@ -2,6 +2,7 @@ import os
 import log
 import time
 from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
 from converter.convert import convert
 
@@ -9,7 +10,6 @@ import tkinter
 from tkinter import scrolledtext
 from tkinter import filedialog
 
-from typing import Dict
 from typing import List
 
 class App:
@@ -41,38 +41,44 @@ class App:
     def increment(self) -> None:
         with self.lock:
             self.count += 1
-            self.label.config(text=f"Finished {self.count} / {self.image_count} Images")
+            self.root.after(0, self.update_status_label)
 
             if self.image_count <= self.count:
                 log.info("CONVERSION COMPLETE")
                 log.info(f"Finished in {(time.time() - self.start_time):.2f} seconds")
-                self.button.config(state=tkinter.NORMAL)
+                self.root.after(0, lambda: self.button.config(state=tkinter.NORMAL))
 
+    def update_status_label(self):
+        self.label.config(text=f"Finished {self.count} / {self.image_count} Images")
 
     def start_conversion(self):
         self.count = 0
         self.start_time = time.time()
         self.button.config(state=tkinter.DISABLED)
 
-        folder_path: str = filedialog.askdirectory()
-        destination: str = os.path.join(folder_path, "tiff")
+        path: str = filedialog.askdirectory()
+        destination: str = os.path.join(path, "tiff")
 
-        log.set_location(destination)
-        log.debug(f"Selected folder \"{folder_path}\"")
+        log.debug(f"Selected folder \"{path}\"")
 
-        if not folder_path or not os.path.exists(folder_path):
+        if not path or not os.path.exists(path):
             log.error("You need to select a valid folder path")
+            self.button.config(state=tkinter.NORMAL)
             return
 
         if not os.path.exists(destination):
             log.debug("Creating TIFF location")
             os.mkdir(destination)
+        log.set_location(destination)
 
-        files: List[str] = os.listdir(folder_path)
+        files: List[str] = os.listdir(path)
         self.image_count: int = len(files)
 
-        for file in files:
-            file_path = os.path.join(folder_path, file)
 
-            thread: Thread = Thread(target=convert, args=(file_path, destination, self.increment), daemon=True)
-            thread.start()
+        Thread(target=self.background, args=(files, path, destination), daemon=True).start()
+
+    def background(self, files, path, destination):
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            for file in files:
+                file_path = os.path.join(path, file)
+                executor.submit(convert, file_path, destination, self.increment)
